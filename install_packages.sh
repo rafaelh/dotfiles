43b76c38611ec93dfce935bc1433c96c7c45c29b
Rafael Hart <rafael.hart9@gmail.com>
2016-11-21 16:29:36 +1100

    More packages

--- install_packages.sh
+++ install_packages.sh
@@ -2,4 +2,4 @@
 
 apt-get update
 apt-get upgrade
-apt-get install git vim inetutils man openssh dnsutils wget tar nmap apt-transport-https bash-completion bsdtar bzip2 curl diffutils dos2unix elinks figlet findutils gnupg gnutls grep gzip lftp linux-man-pages p7zip procps psmisc pwgen python2 rsync sed sharutils strace unrar unzip zip clang python-dev libxml2-dev libffi-dev libxslt-dev
+apt-get install git vim inetutils man openssh dnsutils wget tar nmap apt-transport-https bash-completion bsdtar bzip2 curl diffutils dos2unix elinks figlet findutils gnupg gnutls grep gzip lftp mlocate linux-man-pages p7zip procps psmisc pwgen python2 rsync sed sharutils strace unrar unzip zip clang python-dev libxml2-dev libffi-dev libxslt-dev
