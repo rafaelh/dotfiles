8555d560c991819c5cdd47a90895f53bb34cc930
Rafael Hart <rafael.hart9@gmail.com>
2016-11-28 16:28:02 +1100

    =

--- install_packages.sh
+++
@@ -1,6 +0,0 @@
-# Packages to install:
-
-apt-get update
-apt-get upgrade
-apt-get install git vim-python inetutils man openssh dnsutils fontconfig-utils wget tar nmap apt-transport-https bash-completion bsdtar bzip2 curl diffutils dos2unix elinks figlet findutils gnupg gnutls grep gzip lftp mlocate linux-man-pages p7zip procps psmisc pwgen python2 rsync sed sharutils strace unrar unzip zip clang python-dev libxml2-dev libffi-dev libxslt-dev
-pip install jedi
