f32cc87ad58d8cba760e51f71f81a06cd6a54dc0
Rafael Hart <rafael.hart9@gmail.com>
2017-01-29 18:14:53 +1100

    Converted setup script to python

--- setup.py
+++ setup.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python
 import os
 
-# Symlink dotfiles
+# Set environment variables
 homedir = os.getenv("HOME") + '/'
 repodir = os.getenv("HOME") + '/dot_lin/'
 links = os.listdir(repodir)
-ignore = ['.git', 'README.md']
+ignore = ['.git', 'README.md', 'setup.py', 'setup']
 
+# Remove standard config files
+if os.path.exists('/etc/skel'):
+    basicdotfiles = os.listdir('/etc/skel')
+    for basicdotfile in basicdotfiles:
+        cmdstring = "rm -rf %s%s" % (homedir, basicdotfile)
+        os.system(cmdstring)
+
+# Simlink dotfiles
 for link in links:
-    if link not in ignore:
+    if link not in ignore and not os.path.exists(homedir + '.' + link):
         print "\033[1;32;40m>>> \033[1;37;40mLinking: %s\033[0;37;40m" % link
-        cmdstring = "ln -s %s%s %s.%s" % (homedir, link, repodir, link)
+        cmdstring = "ln -s %s%s %s.%s" % (repodir, link, homedir, link)
+        os.system(cmdstring)
         print cmdstring
 
-# To do
-# Add checks to remove existing files if links are already THEre
-
-
 # Download git plugins
-
-def gitsync( gitrepo ):
-    cmdstring = "git -C %svim/bundle/ clone %s" % (repodir, gitrepo)
-    os.system(cmdstring)
+def gitsync(gitrepo, gitname):
+    if not os.listdir(repodir + 'vim/bundle/' + gitname):
+        print "Syncing %s" % gitname
+        cmdstring = "git -C %svim/bundle/ clone %s" % (repodir, gitrepo)
+        os.system(cmdstring)
     return;
 
-print "Syncing vim plugins"
-gitsync('https://github.com/jiangmiao/auto-pairs')
-gitsync('https://github.com/ajh17/VimCompletesMe')
-
-
-# To do:
-# add 'is directory empty checks' - if os.listdir(work_path) = []:
-#
-# check if gitrepos is created, creat it if not, and add a 'dot_lin' symlink to
-# it
+gitsync('https://github.com/jiangmiao/auto-pairs', 'auto-pairs')
+gitsync('https://github.com/ajh17/VimCompletesMe', 'VimCompletesMe')
