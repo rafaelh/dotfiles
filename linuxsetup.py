cfa9a89b90444111cc98ece7b59e2a01d25728d5
Rafael Hart <rafael.hart9@gmail.com>
2017-06-14 11:47:49 +1000

    Added new plugins

---
+++ linuxsetup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+
+# Need to detect environment
+
+commands = ['apt-get update',
+            'apt-get upgrade -y',
+            ]
+
+for command in commands:
+    cmdstring = command
+    os.system(command)
+
+# Need to add lots of conditional stuff
