971434e2583d3c8741a1ab976f05e05a619df0d4
Rafe Hart <rafael.hart9@gmail.com>
2019-10-05 08:26:10 +1000

    Cleanup

--- setup.py
+++ setup.py
@@ -39,11 +39,6 @@ for link in links:
         cmdstring = "ln -s %s%s %s.%s" % (repodir, link, homedir, link)
         os.system(cmdstring)
 
-# For Docker
-for mount in mounts:
-    cmdstring = "sudo ln -s /mnt/%s /%s" % (mount, mount)
-    os.system(cmdstring)
-
 # Download git plugins
 if not os.path.exists(repodir + 'vim/bundle'):
     cmdstring = "mkdir %s" % repodir + 'vim/bundle'
@@ -53,7 +48,7 @@ def gitsync(gitrepo, gitname):
     if not os.path.exists(repodir + 'vim/bundle'):
         cmdstring = "mkdir %s" % repodir + 'vim/bundle'
     if not os.path.exists(repodir + 'vim/bundle/' + gitname):
-        print("\033[1;32;40m>>> \033[1;37;40mSyncing: %s\033[0;37;0m" % gitname)
+        print("\n\033[1;32;40m>>> \033[1;37;40mSyncing: %s\033[0;37;0m" % gitname)
         cmdstring = "mkdir %s" % repodir + 'vim/bundle/' + gitname
         os.system(cmdstring)
         cmdstring = "git -C %svim/bundle/ clone %s" % (repodir, gitrepo)
