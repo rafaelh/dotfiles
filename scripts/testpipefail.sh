b7766c1d9fde780eaf64c4a1fcedf3b14c58bb53
Rafe Hart <rafael.hart9@gmail.com>
2020-10-09 01:59:07 -0400

    Making error more informative

--- scripts/testpipefail.sh
+++ scripts/testpipefail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -Eeuo pipefail
-trap "echo -e \"\033[1;31m[!] \e[0m Script error occured.\"" ERR
+trap "echo -e \"\033[1;31m[!] \e[0mScript error occured in $0 on line $LINENO.\"" ERR
 
 myfunc()
 {
