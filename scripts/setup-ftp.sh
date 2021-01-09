0a978e7aa1ada70e0c5c6541ab49fcf6aa80a1d1
Rafe Hart <rafael.hart9@gmail.com>
2021-01-09 13:17:58 +1100

    Added Error handling

--- scripts/setup-ftp.sh
+++ scripts/setup-ftp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -Eeuo pipefail
+trap "echo -e \"\033[1;31m[!] \e[0mScript error occured in $0.\"" ERR
+
 groupadd ftpgroup
 useradd -g ftpgroup -d /dev/null -s /etc ftpuser
 pure-pw useradd offsec -u ftpuser -d /ftphome
